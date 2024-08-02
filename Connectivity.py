ed405bd7a5db015ceb71c057c54974f276d7def3
Geet Vashista <148626773+geetvashista@users.noreply.github.com>
2024-08-02 18:59:01 +1200

    Updated Connectivity.py

--- Connectivity.py
+++ Connectivity.py
@@ -15,9 +15,9 @@ def operations_to_perform():    # Change as desired
 
 
 def setup_dependencies():    # Change as desired
-    input_dir = '//home/sahib/Documents/OPM_MEG/data'     # The directory containing files of interest
-    output_dir = '/home/sahib/Documents/OPM_MEG/Graph_outputs/Beta'    # The output directory
-    array_type = 'run-001'     # This is the type of array that will be loaded for further calculations, eg. "participant_conversation_alpha"
+    input_dir = r'C:\Users\em17531\Desktop\OPM_MEG\data'     # The directory containing files of interest
+    output_dir = r'C:\Users\em17531\Desktop\OPM_MEG\Graph_output\Beta'    # The output directory
+    array_type = 'run-002'     # This is the type of array that will be loaded for further calculations, eg. "participant_conversation_alpha"
     target_fb = [13, 30]  # The target frequency band     TODO: set this up so variable can be a this can be list of bands and each generates it's own adjacency matrix
     band = '_beta_'
     fs = 1200    # The sampling frequency
@@ -33,7 +33,7 @@ def prep_data(input_dir, array_type):
             if array_type in os.path.basename(file):
                 target_array = np.load(os.path.join(input_dir, folder, file))
                 target_array = target_array[0:606000, :]
-                data.append(target_array)
+                data.append(target_array.T)
     return np.stack(data, axis=0)
 
 
