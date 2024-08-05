16648c411780319b8df8ae952834026e8bb6d52e
Geet Vashista <148626773+geetvashista@users.noreply.github.com>
2024-08-05 20:12:26 +1200

    Add files via upload

---
+++ Plot_OPM_MEG_graphs.py
@@ -0,0 +1,36 @@
+"""
+A script for the plotting of AAP atlas graph. These regions ARE NOT set to MNI space as yet.
+78 ROI's where used to match with:
+https://direct.mit.edu/imag/article/doi/10.1162/imag_a_00020/117508/Test-retest-reliability-of-the-human-connectome-An
+A number of changes are possible from here such as, communities and highlight_edges.
+"""
+import netplotbrain
+import pandas as pd
+import numpy as np
+import scipy
+
+# Set depanneurs
+mat_voxlox = scipy.io.loadmat(r'')
+adj = np.load(r'')
+hig = np.load(r'')
+
+# pep loc file
+loc = mat_voxlox['voxlox']
+del mat_voxlox
+
+# Plotting
+
+nodes = pd.DataFrame(data={'x': loc[0, :],
+                           'y': loc[1, :],
+                           'z': loc[2, :]
+                           })
+
+netplotbrain.plot(nodes=nodes,
+                  template_style='filled',
+                  edges=adj,
+                  highlight_edges=hig,
+                  view=['LSR'],
+                  title='All edges, sig highlighted',
+                  arrowaxis=None,
+                  highlight_level=0.92,
+                  node_scale=100)
