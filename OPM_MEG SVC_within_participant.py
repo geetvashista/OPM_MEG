edbb05b4be20dc4ac648c183709b609c0400fb84
Geet Vashista <148626773+geetvashista@users.noreply.github.com>
2024-08-29 16:08:37 +1200

    With participant SVC

---
+++ OPM_MEG SVC_within_participant.py
@@ -0,0 +1,105 @@
+import numpy as np
+import bct
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+import time
+
+# Graph calculator function
+def graph_metrics(adj_matrix):  # TODO: Add in stats, maybe nbs? Could use fdc as well?
+    # Strength calculator
+    Strength = []
+    Strength.append(bct.strengths_und(np.nan_to_num(adj_matrix)))
+    Strength = np.array(Strength)
+
+    # Zeroing negative phasing
+    Strength[Strength < 0] = 0
+
+    # Betweenness centrality calculator
+    Betweenness = []
+    Betweenness.append(bct.betweenness_wei(np.nan_to_num(adj_matrix)))
+    Betweenness = np.array(Betweenness)
+
+    # Eigenvector centrality calculator
+    Eigenvector = []
+    Eigenvector.append(bct.eigenvector_centrality_und(np.nan_to_num(adj_matrix)))
+    Eigenvector = np.array(Eigenvector)
+
+    # Clustering calculator
+    Clustering = []
+    Clustering.append(bct.clustering_coef_wu(np.nan_to_num(adj_matrix)))
+    Clustering = np.array(Clustering)
+
+    return Strength, Betweenness, Eigenvector, Clustering
+
+# feature extraction function
+def feature_extraction(r1, r2):
+    # start = time.time()
+    r1_array = []
+    for sample in r1:
+        features = []
+        r1_S, r1_B, r1_E, r1_C = graph_metrics(sample)
+        features.append(r1_S)
+        features.append(r1_B)
+        features.append(r1_E)
+        features.append(r1_C)
+        sample_features = np.array(features)
+        r1_array.append(sample_features)
+
+    r2_array = []
+    for sample in r2:
+        features = []
+        r2_S, r2_B, r2_E, r2_C = graph_metrics(sample)
+        features.append(r2_S)
+        features.append(r2_B)
+        features.append(r2_E)
+        features.append(r2_C)
+        sample_features = np.array(features)
+        r2_array.append(sample_features)
+    # print('\n' + "FEATURE EXECUTION TIME: " + str(time.time() - start) + " sec")
+    print('\n')
+    return np.array(r1_array), np.array(r2_array)
+
+par_result = []
+start = time.time()
+model = make_pipeline(SVC(C=0.1))
+metric = 0
+for i in range(10):
+    # Load data
+    print('Now running participant: ' + str(i))
+    r1 = np.load(r'C:\Users\em17531\Desktop\OPM_MEG\data\Windows_1\Windowed_1.npy')
+    r2 = np.load(r'C:\Users\em17531\Desktop\OPM_MEG\data\Windows_2\Windowed_2.npy')
+
+    # Pick a target band ([theta, alpha, beta, gamma], across axis one)
+    r1 = r1[1, :, :, :, :]
+    r2 = r2[1, :, :, :, :]
+    r1 = r1[i, :, :, :]
+    r2 = r2[i, :, :, :]
+
+    # Extract features
+    run_1_featurs, run_2_featurs = feature_extraction(r1, r2)
+
+    # Join the arrays
+    x = np.concatenate((run_1_featurs, run_2_featurs), axis=0)
+
+    half_len = len(x)//2
+    y = [0] * half_len + [1] * half_len
+
+    # Split data
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    results = []
+    for k in range(78):
+        model.fit(x_train[:, metric, :, k], y_train)
+        results.append(model.score(x_test[:, metric, :, k], y_test))
+    results = np.array(results)
+
+    areas = []
+    for indx, val in enumerate(results):
+        if val >= 0.52:
+            areas.append(indx)
+    model.fit(x_train[:, metric, 0, np.array(areas)], y_train)
+    par_result.append(model.score(x_test[:, metric, 0, np.array(areas)], y_test))
+
+array = np.array(par_result)
+print(array)
+print('\n' + "FINAL EXECUTION TIME: " + str(time.time() - start) + " sec")
