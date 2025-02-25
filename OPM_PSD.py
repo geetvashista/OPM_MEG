42c3cec016022e38a252fac6353aebf798f549d0
Geet Vashista <148626773+geetvashista@users.noreply.github.com>
2025-02-25 19:29:53 +1300

    Update OPM_PSD.py

--- OPM_PSD.py
+++ OPM_PSD.py
@@ -1,9 +1,27 @@
 import numpy as np
 import mne
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-raw_data = np.load('/home/sahib/Documents/OPM_MEG/data/run_1/sub-001_run-001_VE_1_40_Hz_Z.mat_array.npy')
+# Setting some backend parameters
+matplotlib.use('TkAgg')
+
+# Import and organize data
+raw_data = np.load(r'C:\Users\em17531\Desktop\OPM_MEG\data\run_1\sub-001_run-001_VE_1_40_Hz_Z.mat_array.npy')
 raw_data = raw_data.T
 
+# Creating a needed info object
+info = mne.create_info(ch_names=78,
+                       sfreq=1200,
+                       ch_types='eeg'   # This is simply because I don't want to have to deal with multiple sensor
+                       # types
+                       )
+
+data = mne.io.RawArray(raw_data, info)
+data.info['line_freq'] = 50
+
+remove_ch = [1, 2, 24, 28, 32, 33, 34, 45, 40, 41, 63, 67, 71, 72, 73, 74]
 ROI_names = ['Frontal_Sup_Orb_L',
 'Frontal_Med_Orb_L',
 'Frontal_Mid_Orb_L',
@@ -67,25 +85,24 @@ ROI_names = ['Frontal_Sup_Orb_L',
 'Cingulum_Post_R',
 'Insula_R']
 
-info = mne.create_info(ch_names=78,
-                       sfreq=1200,
-                       ch_types='eeg'
-                       )
-
-data = mne.io.RawArray(raw_data, info)
-data.info['line_freq'] = 50
-
-remove_ch = [1, 2, 24, 28, 32, 33, 34, 45, 40, 41, 63, 67, 71, 72, 73, 74]
-
-# Set up ch_name mapping
-temp = [str(i) for i in range(78)]
-for k in remove_ch:
-    del temp[k]
-
 for i in remove_ch:
     data.drop_channels(str(i))
 
+temp = data.ch_names
 ch_names_mapping = dict(zip(temp, ROI_names))
 
 # rename ch_names
 mne.rename_channels(data.info, ch_names_mapping)
+
+# picks= [i for i in range(4)]
+fig = data.compute_psd(fmin=1, fmax=30, average='mean')
+temp = fig.get_data()
+
+epsilon = 1e-10
+to_plot = np.mean(temp, axis=0)
+to_plot = 10 * np.log10(np.maximum(to_plot, epsilon) / 1)
+# to_plot = np.array(hold)
+#
+sns.lineplot(to_plot)
+
+# sns.lineplot(np.mean(temp, axis=0))
