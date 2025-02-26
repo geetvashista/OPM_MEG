5e302b823da751b3053c8eaef2d71a564652a966
Geet Vashista <148626773+geetvashista@users.noreply.github.com>
2025-02-27 00:19:59 +1300

    Update OPM_PSD.py

--- OPM_PSD.py
+++ OPM_PSD.py
@@ -1,5 +1,6 @@
 import numpy as np
 import mne
+import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -7,102 +8,114 @@ import seaborn as sns
 # Setting some backend parameters
 matplotlib.use('TkAgg')
 
-# Import and organize data
-raw_data = np.load(r'C:\Users\em17531\Desktop\OPM_MEG\data\run_1\sub-001_run-001_VE_1_40_Hz_Z.mat_array.npy')
-raw_data = raw_data.T
 
-# Creating a needed info object
-info = mne.create_info(ch_names=78,
-                       sfreq=1200,
-                       ch_types='eeg'   # This is simply because I don't want to have to deal with multiple sensor
-                       # types
-                       )
+def get_mean_psd(file_path):
+    raw_data = np.load(file_path)
+    raw_data = raw_data.T
 
-data = mne.io.RawArray(raw_data, info)
-data.info['line_freq'] = 50
+    ROI_names = ['Frontal_Sup_Orb_L',
+    'Frontal_Med_Orb_L',
+    'Frontal_Mid_Orb_L',
+    'Frontal_Inf_Orb_L',
+    'Frontal_Sup_L' ,
+    'Frontal_Mid_L' ,
+    'Frontal_Inf_Oper_L' ,
+    'Frontal_Inf_Tri_L' ,
+    'Frontal_Sup_Medial_L',
+    'Supp_Motor_Area_L' ,
+    'Paracentral_Lobule_L',
+    'Precentral_L' ,
+    'Rolandic_Oper_L',
+    'Postcentral_L' ,
+    'Parietal_Sup_L' ,
+    'Parietal_Inf_L' ,
+    'SupraMarginal_L',
+    'Angular_L' ,
+    'Precuneus_L' ,
+    'Occipital_Sup_L' ,
+    'Occipital_Mid_L',
+    'Calcarine_L',
+    'Cuneus_L' ,
+    'Lingual_L',
+    'Heschl_L',
+    'Temporal_Sup_L',
+    'Temporal_Mid_L',
+    'Cingulum_Ant_L',
+    'Cingulum_Mid_L',
+    'Cingulum_Post_L',
+    'Insula_L' ,
+    'Frontal_Sup_Orb_R',
+    'Frontal_Med_Orb_R',
+    'Frontal_Mid_Orb_R',
+    'Frontal_Inf_Orb_R',
+    'Frontal_Sup_R',
+    'Frontal_Mid_R',
+    'Frontal_Inf_Oper_R',
+    'Frontal_Inf_Tri_R',
+    'Frontal_Sup_Medial_R',
+    'Supp_Motor_Area_R',
+    'Paracentral_Lobule_R',
+    'Precentral_R',
+    'Rolandic_Oper_R',
+    'Postcentral_R',
+    'Parietal_Sup_R',
+    'Parietal_Inf_R',
+    'SupraMarginal_R',
+    'Angular_R',
+    'Precuneus_R',
+    'Occipital_Sup_R',
+    'Occipital_Mid_R',
+    'Calcarine_R',
+    'Cuneus_R',
+    'Lingual_R',
+    'Heschl_R',
+    'Temporal_Sup_R',
+    'Temporal_Mid_R',
+    'Cingulum_Ant_R',
+    'Cingulum_Mid_R',
+    'Cingulum_Post_R',
+    'Insula_R']
 
-remove_ch = [1, 2, 24, 28, 32, 33, 34, 45, 40, 41, 63, 67, 71, 72, 73, 74]
-ROI_names = ['Frontal_Sup_Orb_L',
-'Frontal_Med_Orb_L',
-'Frontal_Mid_Orb_L',
-'Frontal_Inf_Orb_L',
-'Frontal_Sup_L' ,
-'Frontal_Mid_L' ,
-'Frontal_Inf_Oper_L' ,
-'Frontal_Inf_Tri_L' ,
-'Frontal_Sup_Medial_L',
-'Supp_Motor_Area_L' ,
-'Paracentral_Lobule_L',
-'Precentral_L' ,
-'Rolandic_Oper_L',
-'Postcentral_L' ,
-'Parietal_Sup_L' ,
-'Parietal_Inf_L' ,
-'SupraMarginal_L',
-'Angular_L' ,
-'Precuneus_L' ,
-'Occipital_Sup_L' ,
-'Occipital_Mid_L',
-'Calcarine_L',
-'Cuneus_L' ,
-'Lingual_L',
-'Heschl_L',
-'Temporal_Sup_L',
-'Temporal_Mid_L',
-'Cingulum_Ant_L',
-'Cingulum_Mid_L',
-'Cingulum_Post_L',
-'Insula_L' ,
-'Frontal_Sup_Orb_R',
-'Frontal_Med_Orb_R',
-'Frontal_Mid_Orb_R',
-'Frontal_Inf_Orb_R',
-'Frontal_Sup_R',
-'Frontal_Mid_R',
-'Frontal_Inf_Oper_R',
-'Frontal_Inf_Tri_R',
-'Frontal_Sup_Medial_R',
-'Supp_Motor_Area_R',
-'Paracentral_Lobule_R',
-'Precentral_R',
-'Rolandic_Oper_R',
-'Postcentral_R',
-'Parietal_Sup_R',
-'Parietal_Inf_R',
-'SupraMarginal_R',
-'Angular_R',
-'Precuneus_R',
-'Occipital_Sup_R',
-'Occipital_Mid_R',
-'Calcarine_R',
-'Cuneus_R',
-'Lingual_R',
-'Heschl_R',
-'Temporal_Sup_R',
-'Temporal_Mid_R',
-'Cingulum_Ant_R',
-'Cingulum_Mid_R',
-'Cingulum_Post_R',
-'Insula_R']
+    info = mne.create_info(ch_names=78,
+                           sfreq=1200,
+                           ch_types='eeg'
+                           )
 
-for i in remove_ch:
-    data.drop_channels(str(i))
+    data = mne.io.RawArray(raw_data, info)
+    data.info['line_freq'] = 50
 
-temp = data.ch_names
-ch_names_mapping = dict(zip(temp, ROI_names))
+    remove_ch = [1, 2, 24, 28, 32, 33, 34, 45, 40, 41, 63, 67, 71, 72, 73, 74]
 
-# rename ch_names
-mne.rename_channels(data.info, ch_names_mapping)
+    for i in remove_ch:
+        data.drop_channels(str(i))
 
-# picks= [i for i in range(4)]
-fig = data.compute_psd(fmin=1, fmax=30, average='mean')
-temp = fig.get_data()
+    temp = data.ch_names
+    ch_names_mapping = dict(zip(temp, ROI_names))
 
-epsilon = 1e-10
-to_plot = np.mean(temp, axis=0)
-to_plot = 10 * np.log10(np.maximum(to_plot, epsilon) / 1)
-# to_plot = np.array(hold)
-#
-sns.lineplot(to_plot)
+    # rename ch_names
+    mne.rename_channels(data.info, ch_names_mapping)
 
-# sns.lineplot(np.mean(temp, axis=0))
+    # picks= [i for i in range(4)]
+    fig = data.compute_psd(fmin=1, fmax=30, average='mean')
+    temp = fig.get_data()
+
+    epsilon = 1e-10
+    to_plot = np.mean(temp, axis=0)
+    to_plot = 10 * np.log10(np.maximum(to_plot, epsilon) / 1)
+    # to_plot = np.array(hold)
+    return to_plot
+
+
+def plot(participant):
+    # Get the data we need
+    run_1 = get_mean_psd(f'/home/sahib/Documents/OPM_MEG/data/run_1/sub-00{participant}_run-001_VE_1_40_Hz_Z.mat_array.npy')
+    run_2 = get_mean_psd(f'/home/sahib/Documents/OPM_MEG/data/run_2/sub-00{participant}_run-002_VE_1_40_Hz_Z.mat_array.npy')
+
+    # Plotting
+    df = pd.DataFrame({'Run 1': run_1, 'Run 2': run_2})
+    sns.lineplot(df).set_title(f'Participant {participant}')
+
+
+# __Main__
+
+plot(1)
